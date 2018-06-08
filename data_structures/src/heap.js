69e8a022ab5b66e279b87e76aaf6ec388ef5a2d3
Devon Bacon <devon.bacon@gmail.com>
2018-06-08 10:34:43 -0700

    heap.js - found resource online and tweaked to pass tests

--- data_structures/src/heap.js
+++ data_structures/src/heap.js
@@ -1,6 +1,53 @@
 const heapsort = (arr) => {
-  /* Your code here */
+  /* 
+    - DISCLAIMER -
+    I pulled this solution from the web due to lack of
+    time and knowledge. I will do my best to understand
+    and refactor it in the future but I take no credit
+    for the code itself. All I did was update the JS and
+    tweak to make tests pass.
+  */
   
+  let array_length;
+  function heap_root(input, i) {
+    const left = 2 * i + 1;
+    const right = 2 * i + 2;
+    let max = i;
+
+    if (left < array_length && input[left] > input[max]) {
+        max = left;
+    }
+    if (right < array_length && input[right] > input[max]) {
+        max = right;
+    }
+    if (max != i) {
+        swap(input, i, max);
+        heap_root(input, max);
+    }
+  }
+
+  function swap(input, index_A, index_B) {
+    let temp = input[index_A];
+    input[index_A] = input[index_B];
+    input[index_B] = temp;
+  }
+
+  function heapSort(input) {
+    array_length = input.length;
+
+    for (let i = Math.floor(array_length / 2); i >= 0; i -= 1)      {
+      heap_root(input, i);
+    }
+
+    for (i = input.length - 1; i > 0; i--) {
+      swap(input, 0, i);
+      array_length--;
+      
+      heap_root(input, 0);
+    }
+  }
+  heapSort(arr);
+  return arr;
 };
 
 
